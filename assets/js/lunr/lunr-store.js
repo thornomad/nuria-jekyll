85bb07b9af1576cf52b8a452fbf95e3d25570579
nuriatimm <nuriatimm@users.noreply.github.com>
2020-11-14 17:19:46 +0000

    jekyll build from Action 86d98820054b22abaefef6cddc2d2be333588cd7

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,43 @@
+var store = [{
+        "title": "Your Dads First Post",
+        "excerpt":"Hi Nuria! This is your dad writing your first post. I am sure you are not happy about that at all! As as soon as you learn a bit more you can write your own posts. You are going to have a lot to learn to become a scientist and...","categories": [],
+        "tags": [],
+        "url": "https://www.nuriatimm.com/your-dads-first-post/",
+        "teaser": null
+      },{
+        "title": "Life struggles",
+        "excerpt":"In my life i’ve had struggles, i am sure you had at least one. I will give you some struggles i’ve had, also what i did to solve it. This can help you also if you happend to have the same problem. One day I wanted to have soft and...","categories": [],
+        "tags": [],
+        "url": "https://www.nuriatimm.com/life-struggles/",
+        "teaser": null
+      },{
+        "title": "American doll house",
+        "excerpt":"If your kid has an American doll and they want to to have an American Doll House this is the best place. Equiptment: wall Shelf, cooling towel, pins, decorative lights, American funiture. Steps: pin the cooling towel onto the wall shelf for cool backrounds just for decorated vibes. Put the...","categories": [],
+        "tags": [],
+        "url": "https://www.nuriatimm.com/Amercan-Doll-House/",
+        "teaser": null
+      },{
+        "title": "How to have an enjoyeble life as a kid",
+        "excerpt":"Here is how to be a great kid having a awsome life. First you need to get very comfy. You can put candles in your room to have a homey vibe. Play some relaxing music. Wear some comfy clothes. You can put on a face mask.You can sit in your...","categories": [],
+        "tags": [],
+        "url": "https://www.nuriatimm.com/How-to-have-a-great-life-as-a-kid/",
+        "teaser": null
+      },{
+        "title": "Family pommunicates",
+        "excerpt":"If you have not have much time with your family, this is for you. Things you can do with your family is that you can watch a movie with them. You can play a family game that you will all enjoy. Also you can take turns telling a dream that...","categories": [],
+        "tags": [],
+        "url": "https://www.nuriatimm.com/family/",
+        "teaser": null
+      },{
+        "title": "Covid-Free cookies",
+        "excerpt":"Here is how to make covid free cookies. Ingredients: store bought cookie dough or homemade, food dye, eqiptment: roller, 2 bowls, spoon, plastic wrap Steps: Split the dough to make it have one small amount dough and one big amount of dough in one of the bowls, put food diy...","categories": [],
+        "tags": [],
+        "url": "https://www.nuriatimm.com/how-to-make-a-covid-free-cookie/",
+        "teaser": null
+      },{
+        "title": "Spa",
+        "excerpt":"If you want to have a spa right at home this is for you. Fill the bath tub with water maybe also some bubbles. You can set up some candles. Put on a face mask. Put some rose petals on the bubbles. Turn of the lights and you are done....","categories": [],
+        "tags": [],
+        "url": "https://www.nuriatimm.com/how-to-have-a-spa-day/",
+        "teaser": null
+      }]
